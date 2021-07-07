7bd7042c667cb20cf99f8ec7ae4bf82bf717d5ad
along <wuhl6@mail2.sysu.edu.cn>
2021-07-08 00:41:17 +0800

    test: add basic test case

---
+++ 01/test/test.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const assert = require('assert');
+const statement = require('../statement.js')
+
+describe('statement', function() {
+  it("the statement result should be corresponding to the result.json", function() {
+    let jsonString = fs.readFileSync('./data/invoices.json')
+    const invoices = JSON.parse(jsonString)
+
+    jsonString = fs.readFileSync('./data/plays.json')
+    const plays = JSON.parse(jsonString)
+
+    jsonString = fs.readFileSync('./data/result.json')
+    const standard = JSON.parse(jsonString)
+
+    for (const invoice of invoices) {
+      let result = statement(invoice, plays);
+      assert.equal(result, standard[invoice["customer"]])
+    }
+  });
+});
+
