5bbcc8dde98f4ae41f1ed10c34481d5e9fd8928b
along <wuhl6@mail2.sysu.edu.cn>
2021-07-10 09:51:22 +0800

    refactor: delete unused temporary play

--- 01/statement.js
+++ 01/statement.js
@@ -10,7 +10,7 @@ function statement(invoice, plays) {
           .format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -27,7 +27,7 @@ function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
 
     switch (playFor(aPerformance).type) {
