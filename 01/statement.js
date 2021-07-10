cac0c0c077bb68fc492b4987a907b1943eda84ce
along <wuhl6@mail2.sysu.edu.cn>
2021-07-10 11:19:20 +0800

    refactor: rename function format to usd

--- 01/statement.js
+++ 01/statement.js
@@ -7,12 +7,12 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
         perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owned is ${format(totalAmount / 100)}\n`;
+  result += `Amount owned is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
@@ -53,7 +53,7 @@ function statement(invoice, plays) {
     return result;
   }
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl
         .NumberFormat(
             'en-US',
