708724ba6c79402b5fa993fc4758655249c0325f
along <wuhl6@mail2.sysu.edu.cn>
2021-07-10 11:15:09 +0800

    refactor: remove temporary format with query

--- 01/statement.js
+++ 01/statement.js
@@ -2,12 +2,6 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format =
-      new Intl
-          .NumberFormat(
-              'en-US',
-              {style: 'currency', currency: 'USD', minimumFractionDigits: 2})
-          .format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
@@ -57,6 +51,14 @@ function statement(invoice, plays) {
     if ('comedy' === playFor(perf).type)
       result += Math.floor(perf.audience / 5);
     return result;
+  }
+
+  function format(aNumber) {
+    return new Intl
+        .NumberFormat(
+            'en-US',
+            {style: 'currency', currency: 'USD', minimumFractionDigits: 2})
+        .format(aNumber);
   }
 }
 
